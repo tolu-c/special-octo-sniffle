6e75debff6806a237ad5222609894d98cb5f7b4c
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-02-27 01:26:46 +0100

    applied tailwind css

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold text-blue-800 underline">Hello world!</h1>;
 }
