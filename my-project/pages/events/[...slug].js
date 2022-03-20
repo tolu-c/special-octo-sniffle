57614fc0fa8732606e6b28c3813449072fa3465a
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-03-21 00:26:58 +0100

    filteredEvents

--- my-project/pages/events/[...slug].js
+++ my-project/pages/events/[...slug].js
@@ -1,5 +1,56 @@
+import { useRouter } from "next/router";
+import EventList from "../../components/events/EventList";
+import { getFilteredEvents } from "../../dummy-data";
+
 function FilteredEventsPage() {
-  return <div>FilteredEventsPage</div>;
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+  // console.log(filteredData);
+
+  if (!filteredData) {
+    return <p>Loading...</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <p className="text-red-600 px-4 text-base shadow-lg shadow-red-200 font-medium rounded-md w-2/3 mx-auto text-center my-4 py-1 bg-red-300 border border-red-500">
+        Invalid Filter, Please adjust your values!
+      </p>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <p className="text-amber-900 px-4 text-base shadow-lg shadow-amber-200 font-medium rounded-md w-2/3 mx-auto text-center my-4 py-1 bg-amber-300 border border-amber-500">
+        No Events found!
+      </p>
+    );
+  }
+
+  return (
+    <div className="bg-teal-100 h-screen flex justify-center items-center">
+      <EventList items={filteredEvents} />
+    </div>
+  );
 }
 
 export default FilteredEventsPage;
