65ab054d7c6565eede767378a889415c9c7898a8
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-03-20 18:22:47 +0000

    added icons

--- my-project/components/events/EventItem.js
+++ my-project/components/events/EventItem.js
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import Button from '../ui/Button'
+import Button from "../ui/Button";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -12,6 +11,8 @@ function EventItem(props) {
 
   const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
+
+  // icons => location, calendar, arrow-right
 
   return (
     <li className="border rounded-md h-52 shadow-lg grid grid-cols-5 overflow-hidden">
@@ -26,11 +27,42 @@ function EventItem(props) {
             {title}
           </h2>
           <div>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
             <time className="font-bold text-base text-stone-500">
               {humanReadableDate}
             </time>
           </div>
           <div className="mt-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
             <address className="text-base font-medium">
               {formattedAddress}
             </address>
@@ -38,7 +70,24 @@ function EventItem(props) {
         </div>
 
         <div className="text-right mt-10">
-          <Button link={exploreLink}>Explore events</Button>
+          <Button link={exploreLink}>
+            <span>
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                />
+              </svg>
+            </span>
+            <span>Explore events</span>
+          </Button>
         </div>
       </div>
     </li>
