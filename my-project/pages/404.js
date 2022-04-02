a7bc1b8bb720497eaabe8d40eeaed66b4243fc8a
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-02 17:51:30 +0100

    styled 404

--- my-project/pages/404.js
+++ my-project/pages/404.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function PageNotFound() {
-    return <h1>this page is missing</h1>
+  return (
+    <div className="flex flex-col justify-center items-center p-3 border border-red-400 bg-red-100 my-6 mx-auto w-auto max-w-2xl">
+      <h1>sorry, we could not find this page.</h1>
+      <ul>
+        <li>make sure the url is correct.</li>
+      </ul>
+      <Link href="/">
+        <p className="capitalize text-lg font-medium hover:underline text-gray-400 hover:text-gray-600">home</p>
+      </Link>
+    </div>
+  );
 }
