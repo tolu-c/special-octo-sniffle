200e3e620a89c9538953fe62ab13efc0a7145e18
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-03 23:51:03 +0100

    getstaticprops with dummy data

--- next-project/pages/index.js
+++ next-project/pages/index.js
@@ -1,18 +1,29 @@
-import React from "react";
+import fs from "fs/promises";
+import path from "path";
 
-function Home() {
+function Home(props) {
+  const { products } = props;
   return (
     <div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
       </ul>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  return { props };
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData)
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 export default Home;
