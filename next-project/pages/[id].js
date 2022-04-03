9907a6078c6461442f1d419f3d438d17a31bdf32
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-04 00:36:40 +0100

    introduction to getStaticPaths

--- next-project/pages/[id].js
+++ next-project/pages/[id].js
@@ -29,3 +29,14 @@ export async function getStaticProps(context) {
     },
   };
 }
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "p1" } },
+      { params: { id: "p2" } },
+      { params: { id: "p3" } },
+    ],
+    fallback: false,
+  };
+}
