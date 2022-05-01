e981708a3ed838775c0fb3ccef2a3d83b0927fdc
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-01 11:56:40 +0100

    using useSWR

--- next-project/pages/last-sales.js
+++ next-project/pages/last-sales.js
@@ -1,35 +1,62 @@
 import { useEffect, useState } from "react"
+import useSWR from 'swr'
 
 export default function LastSalesPage() {
   const [sales, setSales] = useState()
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
+
+  const { data, error } = useSWR('https://nextjscourse-59cfb-default-rtdb.firebaseio.com/sales.json')
+  console.log(data);
 
   useEffect(() => {
-    setIsLoading(true)
-    fetch('https://nextjscourse-59cfb-default-rtdb.firebaseio.com/sales.json').then(response => response.json()).then(data => {
-      const transformData = []
+    if (data) {
+      const transformedSales = []
+
       for (const key in data) {
-        transformData.push({
+        transformedSales.push({
           id: key,
           username: data[key].username,
-          volume: data[key].volume
-        })
+          volume: data[key].volume,
+        });
       }
 
-      setSales(transformData)
-      setIsLoading(false)
-    })
-  }, [])
+      setSales(transformedSales)
+    }
+  }, [data])
+  console.log(sales);
 
-  if (isLoading) {
-    return <p>Loading... Just hang in there for a second.</p>
+  // useEffect(() => {
+  //   setIsLoading(true)
+  //   fetch('https://nextjscourse-59cfb-default-rtdb.firebaseio.com/sales.json').then(response => response.json()).then(data => {
+  //     const transformData = []
+  //     for (const key in data) {
+  //       transformData.push({
+  //         id: key,
+  //         username: data[key].username,
+  //         volume: data[key].volume
+  //       })
+  //     }
+
+  //     setSales(transformData)
+  //     setIsLoading(false)
+  //   })
+  // }, [])
+
+  if (error) {
+    return <p>Failed to load data.</p>
   }
 
-  if (!sales) {
-    return <p>No sales here.</p>
+  if (!data || !sales) {
+    return <p>Loading your data.. Hang in there for a second.</p>
   }
-  
-  return <ul>
-    {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
-  </ul>
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  )
 }
