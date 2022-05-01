83de7a8b31169adb9b3a4ef4e7de1b0fb681e8f3
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-01 10:19:03 +0100

    getserversideprops

---
+++ next-project/pages/user-profile.js
@@ -0,0 +1,12 @@
+export default function UserProfilePage(props) {
+  const { username } = props
+  return <h1>{username}</h1>
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      username: 'Max'
+    }
+  }
+}
