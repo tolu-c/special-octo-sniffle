6bf663b2e0896ba6f06e418ad6a8029dcede4acb
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-09-21 08:16:17 +0000

    frontend form

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,7 +1,36 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const enteredEmail = emailRef.current.value;
+    const enteredFeedback = feedbackRef.current.value;
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form action="#">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">feedback</label>
+          <textarea
+            name="feedback"
+            id="feedback"
+            ref={feedbackRef}
+            cols="10"
+            rows="5"
+          ></textarea>
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
     </div>
   );
 }
