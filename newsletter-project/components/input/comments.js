daa3584e69692e7d91d0c2d3ea7028c095e9315f
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-10-10 22:02:11 +0100

    added notification to comments

--- newsletter-project/components/input/comments.js
+++ newsletter-project/components/input/comments.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { NotificationContext } from "../../store/notification-context";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
@@ -7,15 +8,26 @@ import classes from "./comments.module.css";
 function Comments({ eventID }) {
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const notificationCtx = useContext(NotificationContext);
 
   useEffect(() => {
     if (showComments) {
+      notificationCtx.showNotification({
+        title: "Getting your comments",
+        message: "Fetching your comments from our database",
+        status: "pending",
+      });
       fetch(`/api/comments/${eventID}`)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
           setComments(data?.comments);
+          notificationCtx.showNotification({
+            title: "Comment is ready",
+            message: "Comments served!",
+            status: "success",
+          });
         });
     }
   }, [showComments]);
@@ -25,6 +37,11 @@ function Comments({ eventID }) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: "Comment",
+      message: "Adding your comment",
+      status: "pending",
+    });
     fetch("/api/comments/" + eventID, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -33,7 +50,13 @@ function Comments({ eventID }) {
       },
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Comment",
+          message: "Comments Added!",
+          status: "success",
+        });
+      });
   }
 
   return (
@@ -41,6 +64,9 @@ function Comments({ eventID }) {
       <button onClick={toggleCommentsHandler}>
         {showComments ? "Hide" : "Show"} Comments
       </button>
+      {notificationCtx.notification?.status === "pending" && (
+        <p>Loading comments...</p>
+      )}
       {showComments && <NewComment onAddComment={addCommentHandler} />}
       {showComments && <CommentList items={comments} />}
     </section>
