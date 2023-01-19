b8126e3bf6c05020d365525764320d24890978b9
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-01-19 22:26:48 +0100

    ...

--- next-a-u-t-h/components/profile/user-profile.js
+++ next-a-u-t-h/components/profile/user-profile.js
@@ -1,8 +1,14 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession } from "next-auth/react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
