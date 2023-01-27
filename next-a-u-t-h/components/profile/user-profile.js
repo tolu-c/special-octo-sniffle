8f1f011bd9d329c92e836d05946c9b196860ab0a
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-01-27 13:33:36 +0100

    server side page guards

--- next-a-u-t-h/components/profile/user-profile.js
+++ next-a-u-t-h/components/profile/user-profile.js
@@ -1,31 +1,7 @@
-import { getSession, useSession } from "next-auth/react";
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
-import { useEffect, useState } from "react";
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
-  // Redirect away if NOT auth
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
-
-  const { data: session, status } = useSession();
-  console.log({ session, status });
-
-  // if (status === "loading") {
-  //   return <p>Loading...</p>;
-  // }
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
