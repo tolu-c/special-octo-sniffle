69067536f5e4f0f5f8bc3aba87224f9751e19a48
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-02-16 16:57:47 +0100

    change password logic

--- next-a-u-t-h/pages/api/user/change-password.js
+++ next-a-u-t-h/pages/api/user/change-password.js
@@ -1,5 +1,7 @@
 // /api/user/change-password
 import { getSession } from "next-auth/react";
+import { connectDB } from "../../../lib/db";
+import { hashPassword, verifyPassword } from "../../../lib/auth";
 
 export default async function handler(req, res) {
   if (req.method !== "PATCH") {
@@ -14,4 +16,38 @@ export default async function handler(req, res) {
       .json({ message: "You are not authorized to perform this action!" });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectDB();
+  const db = client.db();
+  const user = await db.collection("members").findOne({ email: userEmail });
+
+  // checks if the user sending the request exists
+  if (!user) {
+    res.status(404).json({ message: "User not found!" });
+    client.close();
+    return;
+  }
+
+  const userCurrentPassword = user.password;
+  const passwordIsSame = await verifyPassword(oldPassword, userCurrentPassword);
+
+  // checks if oldPassword matches currentPassword
+  if (!passwordIsSame) {
+    res.status(403).json({ message: "password does not match" });
+    client.close();
+    return;
+  }
+
+  // set password to new password
+  const result = await user.updateOne(
+    { email: userEmail },
+    { $set: { password: hashPassword(newPassword) } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated successfully" });
 }
